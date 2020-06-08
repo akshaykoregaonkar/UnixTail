a9ff5e510231a1f7e2c2c930f2bd23537b45e7f4
a_koregaonkar <akshay.koregaonkar@gmail.com>
2020-06-08 11:02:18 +0100

    minor refactoring

--- src/com/akshay/tests/TailPrinterTest.java
+++ src/com/akshay/tests/TailPrinterTest.java
@@ -3,6 +3,7 @@ package com.akshay.tests;
 import com.akshay.TailPrinter;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -35,6 +36,7 @@ class TailPrinterTest {
         System.setErr(originalErr);
     }
 
+    @DisplayName("If file size exeeds N, then print all lines from file")
     @Test
     void printAllLinesOfExtraSmallFileWhenNExceedsFileSize() {
         StringWriter expectedStringWriter = new StringWriter();
@@ -54,6 +56,7 @@ class TailPrinterTest {
         assertEquals(expected, outContent.toString());
     }
 
+    @DisplayName("Print last line of a file")
     @Test
     void printLastLineForSmallFile() {
         TailPrinter tailPrinter = new TailPrinter(Arrays.asList(getPath("small_sample.txt")), 1);
@@ -61,6 +64,7 @@ class TailPrinterTest {
         assertEquals("line 20\r\n", outContent.toString());
     }
 
+    @DisplayName("Print small file with a default N of 10")
     @Test
     void printSmallFileWithDefaultSetting() {
         StringWriter expectedStringWriter = new StringWriter();
@@ -86,49 +90,51 @@ class TailPrinterTest {
         assertEquals(expected, outContent.toString());
     }
 
+    // Use the code in the Main function to generate large_sample.txt and extra_large_sample.txt
 
-        // Use the code in the Main function to generate large_sample.txt and extra_large_sample.txt
+    @DisplayName("Print a large file (~12 MB) with an N of 5")
+    @Test
+    void printLast5LinesOfLargeFile() {
+        StringWriter expectedStringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(expectedStringWriter);
 
-        @Test
-        void printLast5LinesOfLargeFile() {
-            StringWriter expectedStringWriter = new StringWriter();
-            PrintWriter printWriter = new PrintWriter(expectedStringWriter);
+        printWriter.println("line 999996");
+        printWriter.println("line 999997");
+        printWriter.println("line 999998");
+        printWriter.println("line 999999");
+        printWriter.println("line 1000000");
+        printWriter.close();
 
-            printWriter.println("line 999996");
-            printWriter.println("line 999997");
-            printWriter.println("line 999998");
-            printWriter.println("line 999999");
-            printWriter.println("line 1000000");
-            printWriter.close();
+        String expected = expectedStringWriter.toString();
 
-            String expected = expectedStringWriter.toString();
+        TailPrinter tailPrinter = new TailPrinter(Arrays.asList(getPath("large_sample.txt")), 5);
+        tailPrinter.printTail();
 
-            TailPrinter tailPrinter = new TailPrinter(Arrays.asList(getPath("large_sample.txt")), 5);
-            tailPrinter.printTail();
+        assertEquals(expected, outContent.toString());
+    }
 
-            assertEquals(expected, outContent.toString());
-        }
+    @DisplayName("Print an extra large file (~1.4 GB) with an N of 5")
+    @Test
+    void printLast5LinesOfExtraLargeFile() {
+        StringWriter expectedStringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(expectedStringWriter);
 
-        @Test
-        void printLast5LinesOfExtraLargeFile() {
-            StringWriter expectedStringWriter = new StringWriter();
-            PrintWriter printWriter = new PrintWriter(expectedStringWriter);
+        printWriter.println("line 99999996");
+        printWriter.println("line 99999997");
+        printWriter.println("line 99999998");
+        printWriter.println("line 99999999");
+        printWriter.println("line 100000000");
+        printWriter.close();
 
-            printWriter.println("line 99999996");
-            printWriter.println("line 99999997");
-            printWriter.println("line 99999998");
-            printWriter.println("line 99999999");
-            printWriter.println("line 100000000");
-            printWriter.close();
+        String expected = expectedStringWriter.toString();
 
-            String expected = expectedStringWriter.toString();
+        TailPrinter tailPrinter = new TailPrinter(Arrays.asList(getPath("extra_large_sample.txt")), 5);
+        tailPrinter.printTail();
 
-            TailPrinter tailPrinter = new TailPrinter(Arrays.asList(getPath("extra_large_sample.txt")), 5);
-            tailPrinter.printTail();
+        assertEquals(expected, outContent.toString());
+    }
 
-            assertEquals(expected, outContent.toString());
-        }
-
+    @DisplayName("Print multiple files with a default N of 10")
     @Test
     void printMultipleFilesWithDefaultSetting() {
         StringWriter expectedStringWriter = new StringWriter();
